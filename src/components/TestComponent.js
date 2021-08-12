6256eb3edfe8e3377c71f5c442d360b5305341d5
allendai1 <65299178+allendai1@users.noreply.github.com>
2021-08-12 13:30:24 -0400

    '.'

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -3,7 +3,7 @@ import "../styles/test.scss"
 export default function TestComponent() {
     return (
         <div className="test">
-            THIS IS A TEST COMPONENT
+            THIS IS A TEST COMPONENT123
         </div>
     )
 }
