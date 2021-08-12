dab8f5bf868c58bd7b93e27b0b8205cc1c00d3c7
allendai1 <65299178+allendai1@users.noreply.github.com>
2021-08-12 17:16:20 -0400

    '.'

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import "../styles/main.scss";
 import SearchBar from "../components/SearchBar";
 export default function index() {
+  function darkMode(){
+    document.documentElement.setAttribute("mode","dark");
+  }
 	return (
 		<div className="home">
 			<div className="title-container">
 				<span className="title">Manga</span>
 				<span className="title">Mirror</span>
         <SearchBar/>
+        {/* <button onClick={darkMode}>Dark mode</button> */}
 			</div>
 		</div>
 	);
