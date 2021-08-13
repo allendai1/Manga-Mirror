e371b25aee0db72dc5def778752901d7790e5a48
allendai1 <65299178+allendai1@users.noreply.github.com>
2021-08-12 21:29:17 -0400

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,8 @@
-import React from "react";
-import "../styles/main.scss";
-import SearchBar from "../components/SearchBar";
+import React from 'react'
+import { Router } from "@reach/router";
+import Home from './home';
 export default function index() {
-  function darkMode(){
-    document.documentElement.setAttribute("mode","dark");
-  }
 	return (
-		<div className="home">
-			<div className="title-container">
-				<span className="title">Manga</span>
-				<span className="title">Mirror</span>
-        <SearchBar/>
-        {/* <button onClick={darkMode}>Dark mode</button> */}
-			</div>
-		</div>
-	);
+		<div>index.js</div>
+	)
 }
